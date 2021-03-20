c329a4996194bc6d25db7ec48136c92b09124c7e
rev <therealrevans@gmail.com>
2021-03-20 13:39:41 -0500

    filepath fix

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const AWS = require('aws-sdk');
 const Discord = require('discord.js');
-import saveToDynamo from './dbActions.js';
+import {saveToDynamo} from './dbActions.js';
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
 
