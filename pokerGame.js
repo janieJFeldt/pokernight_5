ca77c24cb7241fc1d669288fc1ec3cf7ba59a67f
rev <therealrevans@gmail.com>
2021-03-22 19:34:53 -0500

    draw cards

--- pokerGame.js
+++ pokerGame.js
@@ -33,7 +33,7 @@ let drawCard = deck => {
     var rand = Math.random(deck.length-1);
     const suit = [deck[Math.round(rand)]];
     var rand2 = Math.random(suit.length-1)
-    return suit[Math.round(rand2)];
+    return suit.splice(Math.round(rand2),suit.indexOf(',',Math.round(rand2)));
 }
 
 module.exports ={
@@ -48,7 +48,7 @@ module.exports ={
         let card2 = drawCard(deck);
         deck= deck.filter(x=> x !== card2);
 
-        message.reply('\n'+ card1 + '\n' + card2);
+        message.reply('\n Hand:\n'+ card1 + '\n' + card2);
 
         message.reply('\n Remaining cards: ' + deck);
     }
