63a786d9509410fda3b82d720000586b3af800e1
rev <therealrevans@gmail.com>
2021-03-22 19:40:14 -0500

    draw cards

--- pokerGame.js
+++ pokerGame.js
@@ -1,17 +1,17 @@
 let cards = [
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    13
+    ' 1',
+    ' 2',
+    ' 3',
+    ' 4',
+    ' 5',
+    ' 6',
+    ' 7',
+    ' 8',
+    ' 9',
+    '10',
+    '11',
+    '12',
+    '13'
 ]
 
 let suits = [
@@ -25,15 +25,14 @@ let getDeck = () => {
     let newDeck = [];
     suits.forEach(x => newDeck.push(cards.map(y=> x+y)));
 
-    return newDeck;
+    return newDeck.join();
 
 }
 
 let drawCard = deck => {
-    var rand = Math.random(deck.length-1);
-    const suit = [deck[Math.round(rand)]];
-    var rand2 = Math.random(suit.length-1)
-    return suit.splice(Math.round(rand2),suit.indexOf(',',Math.round(rand2)));
+    var rand = Math.round(Math.random(deck.length-1));
+
+    return deck.splice(rand,deck.indexOf(','),2);
 }
 
 module.exports ={
@@ -44,9 +43,7 @@ module.exports ={
 
         message.reply('\nDrawing two cards...');
         let card1 = drawCard(deck);
-        deck= deck.filter(x=> x !== card1);
         let card2 = drawCard(deck);
-        deck= deck.filter(x=> x !== card2);
 
         message.reply('\n Hand:\n'+ card1 + '\n' + card2);
 
