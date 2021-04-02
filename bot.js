d5c20ea0cb186ebfb9b43cb38fa5fadec036b773
rev <therealrevans@gmail.com>
2021-04-02 15:35:53 -0500

    fix: get item from dynamo

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ const decipherCommand = async message => {
       case('start'):
       
       let deck = poker.getDeck();
-      poker.createGame(message,deck,(message,deck) => poker.startPokerGame(message,deck));
+      poker.createGame(message,deck,(result) => poker.startPokerGame(result.message,result.deck));
       
       break;
     }
