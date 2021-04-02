9b601947367fe9f74d1ffc0201f29bd47a832acf
rev <therealrevans@gmail.com>
2021-04-02 16:27:32 -0500

    fix: add constants

---
+++ constants.js
@@ -0,0 +1,36 @@
+
+let cards = [
+    'Ace',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '10',
+    'Jack',
+    'Queen',
+    'King'
+]
+
+let suits = [
+    '<:clobz:823753747300614214>',
+    '<:dimmadimonds:823753747372703785>',
+    '<:hertz:823753747574292521>',
+    '<:spudz:823753747385286676>'
+]
+
+let hands = [
+    'high card',
+    'pair',
+    'two pair',
+    'three of a kind',
+    'straight',
+    'flush',
+    'full house',
+    'four of a kind',
+    'straight flush',
+    'royal flush'
+]
