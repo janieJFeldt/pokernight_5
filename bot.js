d6759306dcd2298c1b6a1c8a5e0007006a5aec9c
rev <therealrevans@gmail.com>
2021-04-02 16:48:04 -0500

    fix river

--- bot.js
+++ bot.js
@@ -45,7 +45,9 @@ const decipherCommand = async message => {
         poker.startPokerGame(message,deck);
       break;
       case('start'):
-      await poker.createGame(message,deck, (message2,newDeck2) => poker.startPokerGame(message2,newDeck2, (message,db,docClient)=>db.scanFromDynamo(message,docClient,'pokerGame')
+      await poker.createGame(message,deck, (message2,newDeck2) =>
+          await poker.startPokerGame(message2,newDeck2, (message,db,docClient)=>
+              await db.scanFromDynamo(message,docClient,'pokerGame')
       ));
       
       break;
