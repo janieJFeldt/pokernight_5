4a6398a5ed63ecd462f79b1c34698644b46f88f3
rev <therealrevans@gmail.com>
2021-04-15 19:28:50 -0500

    fix: add dealer to docdb

--- pokerGame.js
+++ pokerGame.js
@@ -96,6 +96,7 @@ module.exports ={
         // getRiverCard(message, river,4);
         
        // message.channel.send('You had\n' +card1 + '\n' + card2);
+       console.log(JSON.stringify(riverCards));
         let dealerData = `{ "Hand" : ${JSON.stringify(riverCards)} }`;
        await db.saveToDynamo(docClient,'DEALER','pokerGame', JSON.parse(dealerData));
        
