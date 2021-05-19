600124e3d881acf16c8e3dc3708e538f5c50352e
janieJFeldt <84038972+janieJFeldt@users.noreply.github.com>
2021-05-19 11:54:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,24 +18,26 @@ const decipherCommand = async message => {
   if (message.content.startsWith('!')){
     switch(message.content.substr(1)){
       case('start'):{
-       fetch("https://discord.com/api/v8/channels/${message.channel.id}/invites", {
-                    method: "POST",
-                    body: JSON.stringify({
-                        max_age: 86400,
-                        max_uses: 0,
-                        target_application_id: "755827207812677713",  // Youtube Together: 755600276941176913 | Poker Night: 755827207812677713 | Betrayal.io: 773336526917861400 | Fishington.io: 814288819477020702 
-                        target_type: 2,
-                        temporary: false,
-                        validate: null
-                    }),
-                    headers: {
-                        "Authorization": "Bot "+TOKEN,
-                        "Content-Type": "application/json"
-                    }
-                }).then(b => {
-         var j=b.json();
-         message.reply("[Click to open Poker Night in ${message.channel.name}](<https://discord.gg/${j.code}>)");
-       });
+       if (message.mentions.channels.size >0) {
+         fetch("https://discord.com/api/v8/channels/${message.mentions.members.first().id}/invites", {
+                      method: "POST",
+                      body: JSON.stringify({
+                          max_age: 86400,
+                          max_uses: 0,
+                          target_application_id: "755827207812677713",  // Youtube Together: 755600276941176913 | Poker Night: 755827207812677713 | Betrayal.io: 773336526917861400 | Fishington.io: 814288819477020702 
+                          target_type: 2,
+                          temporary: false,
+                          validate: null
+                      }),
+                      headers: {
+                          "Authorization": "Bot "+TOKEN,
+                          "Content-Type": "application/json"
+                      }
+                  }).then(b => {
+           var j=b.json();
+           message.reply("[Click to open Poker Night in ${message.mentions.members.first().name}](<https://discord.gg/${j.code}>)");
+         });
+       }
       }
       break;
     }
